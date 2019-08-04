81bc72e45fa3d846e6fdce7e81125ceeeef07b5d
Robert Marinho <robstermarinho@gmail.com>
2019-08-04 17:03:44 -0300

    Validate file field

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,7 +1,11 @@
+import * as Yup from 'yup';
 import File from '../models/File';
 
 class FileController {
   async store(req, res) {
+    if (!req.file) {
+      return res.status(400).json({ error: 'File is missing.' });
+    }
     const { originalname: name, filename: path } = req.file;
     const file = await File.create({
       name,
